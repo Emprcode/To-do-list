4a871f27e023bea21c6b6e7decdf5db7f42964b2
Narayan Aryal <aryal.coding@gmail.com>
2022-11-04 22:20:27 +1100

    more changes made

--- src/Utility.js
+++ src/Utility.js
@@ -3,6 +3,7 @@ const str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
 
 export const randomString = () => {
     let _id = "";
+    
 
     for(let i = 0; i<length; i++){
         _id +=  str[  Math.floor( Math.random() * str.length)]
